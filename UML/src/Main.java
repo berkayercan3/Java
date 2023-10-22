411b1ab032d6036f85fb94d7dc43e74744f2f8ce
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-10-22 16:05:33 +0300

    Boş

---
+++ UML/src/Main.java
@@ -0,0 +1,13 @@
+
+class Animal {
+    private String name;
+    private int id;
+    private int age;
+    public void setName(String name){
+        this.name=name;
+    }
+    public void eat() {
+        System.out.println("Eating");
+    }
+}
+
