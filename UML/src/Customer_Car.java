46e9b46c2e034d3b5ad835378dbd9bec5d2d67e8
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2023-10-22 15:44:02 +0300

    UML types of diagrams
    Association (İlişki): İlişki, sınıflar arasındaki bağımlılığı veya işbirliğini ifade eder. İki sınıf arasındaki ilişki, sınıfların birbiriyle nasıl etkileşimde bulunduğunu ve veri alışverişi yaptığını gösterir.
    
    İnheritance (Kalıtım): Kalıtım, bir sınıfın (alt sınıf veya türetilmiş sınıf) başka bir sınıftan (üst sınıf veya ana sınıf) özellikleri ve metotlarını miras aldığı bir ilişkiyi ifade eder. Kalıtım ilişkisi, bir sınıfın başka bir sınıfın tüm özelliklerini ve davranışlarını devraldığı anlamına gelir. Alt sınıf, üst sınıfın özelliklerini ve metotlarını kullanabilir ve bunları genişletebilir veya değiştirebilir.
    
    Realization (Gerçekleştirme): Gerçekleştirme ilişkisi, bir arayüzün veya soyut bir sınıfın, bu arayüzü veya sınıfı uygulayan bir sınıf tarafından gerçekleştirildiği ilişkiyi ifade eder. Gerçekleştirme ilişkisi, bir sınıfın belirli metotları uygulayarak veya belirli arayüzlerin tüm yöntemlerini gerçekleştirerek bir arayüzü veya soyut bir sınıfı uyguladığını gösterir. Gerçekleştirme ilişkisi, bir çizgi ile bir açıklık (uzun üçgen) ile gösterilir.
    
    Örnek: Bir "Köpek" sınıfı, "Havlayabilir" adlı bir arayüzü gerçekleştirir, bu nedenle "Köpek" sınıfı "Havlayabilir" arayüzünü uygular.
    
    Dependency (Bağımlılık): Bağımlılık ilişkisi, bir sınıfın diğer bir sınıfa olan bağımlılığını ifade eder. Bir sınıfın bir diğerini kullanması veya bir metodu çağırması durumunda bağımlılık ilişkisi vardır. Bağımlılık ilişkisi, oklarla gösterilir ve oklar bağımlı sınıftan bağımlı sınıfa doğru yönlendirilir.
    
    Örnek: Bir "Müşteri" sınıfı, bir "Sipariş" sınıfına bağımlıdır, çünkü müşteriler siparişleri yaparlar.
    
    Aggregation (Bütünleme): Bütünleme ilişkisi, bir bütünün parçalardan oluştuğunu ve bu parçaların bağımsız olarak var olabileceğini ifade eder. Bütünleme, genellikle bir elmas şeklinde gösterilir ve bütün (whole) sınıfı temsil eden sınıfa doğru bir çizgi ile bağlanır.
    
    Örnek: Bir "Üniversite" sınıfı, birçok "Fakülte" sınıfını içerir, ancak fakülteler ayrı ayrı var olabilirler.
    
    Composition (Bileşim): Bileşim ilişkisi de bir bütünün parçalardan oluştuğunu ifade eder, ancak bu ilişkide parçalar bütünden bağımsız olarak var olamazlar. Yani, bütün (whole) sınıfı, parça (part) sınıfının yaşam döngüsünü kontrol eder. Bileşim ilişkisi, bütün sınıfı temsil eden sınıfa doğru bir siyah elmas şekli ile gösterilir.
    
    Örnek: Bir "Ev" sınıfı, "Oda" sınıfından oluşur ve odalar evden bağımsız olarak var olamazlar.

---
+++ UML/src/Customer_Car.java
@@ -0,0 +1,10 @@
+public class Customer{
+    private String address;
+    private String contactNumber;
+    private String name;
+}
+
+public class Car{
+    private String modelNumber;
+    private Customer owner;
+}
---
+++ UML/src/Customer_Car2.java
@@ -0,0 +1,11 @@
+public class Customer{
+    private String address;
+    private String contactNumber;
+    private String name;
+    private Car car;
+}
+
+public class Car{
+    private String modelNumber;
+    private Customer owner;
+}
